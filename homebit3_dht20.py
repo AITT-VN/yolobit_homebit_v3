a0fd11debefdedfdaa3d1d9bc3cbf8a8074c2afc
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-09-21 13:39:18 +0700

    Update homebit3_dht20.py

--- homebit3_dht20.py
+++ homebit3_dht20.py
@@ -1,12 +1,23 @@
-from machine import I2C
+from machine import SoftI2C
 from time import sleep_ms
 class DHT20(object):
     def __init__(self, i2c):
         self.i2c = i2c
-        if (self.dht20_read_status() & 0x80) == 0x80:
-            self.dht20_init()  
+        try:
+          (self.dht20_read_status() & 0x80) == 0x80
+          try:
+            self.dht20_init()
+          except:
+            pass
+        except:
+            self.sensor_status = 0
+            print('sensor not founded!')
+        else:
+          self.sensor_status = 1 
+          print('sensor founded!')
             
     def read_dht20(self):
+      if self.sensor_status == 1 :
         self.i2c.writeto(0x38, bytes([0xac,0x33,0x00]))
         sleep_ms(80)
         cnt = 0
@@ -20,9 +31,17 @@ class DHT20(object):
         for i in data[:]:
             n.append(i)
         return n
+      else:
+        return 0 
         
-    def dht20_read_status(self):
+    def dht20_read_status(self, data = 0):
+      self.data = data
+      try:
         data = self.i2c.readfrom(0x38, 1, True)
+      except:
+        self.sensor_status = 0 
+        print('sensor not founded! Please connect sensor')
+      else:
         return data[0]
     
     def dht20_init(self):
@@ -42,6 +61,7 @@ class DHT20(object):
         return crc
     
     def dht20_temperature(self):
+      if self.sensor_status == 1 :
         data = self.read_dht20()
         Temper = 0
         if 1:
@@ -51,9 +71,11 @@ class DHT20(object):
             Temper = Temper & 0xfffff
             Temper = (Temper * 200 * 10 / 1024 / 1024 - 500)/10
         return round(Temper, 1)
+      else:
+        return round(0, 1)
 
     def dht20_humidity(self):
-        
+      if self.sensor_status == 1 :
         data = self.read_dht20()
         humidity = 0
         if 1:
@@ -63,4 +85,6 @@ class DHT20(object):
             humidity = humidity >> 4
             humidity = (humidity * 100 * 10 / 1024 / 1024)/10
         return round(humidity, 1)
-
+      else:
+        return round(0,1)
+        
